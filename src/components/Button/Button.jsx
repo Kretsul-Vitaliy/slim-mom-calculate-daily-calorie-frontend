454d0b1ed06906923f8db513438c918b7246faa2
AliVik <85441270+AliVik@users.noreply.github.com>
2022-07-08 21:22:32 +0300

    add logic for universal button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,31 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ButtonStyled } from './Button.styled';
+import styled from "styled-components";
+import '../../css/variables.css'
 
-export default function Button({
-  children,
-  disabled,
-  active,
-  onClick,
-  ...attrs
-}) {
-  return (
-    <ButtonStyled {...attrs} disabled={disabled} onClick={onClick}>
-      {children}
-    </ButtonStyled>
-  );
-}
 
-Button.propTypes = {
-  children: PropTypes.node,
-  onClick: PropTypes.func.isRequired,
-  disabled: PropTypes.bool,
-  active: PropTypes.bool,
-};
+// В компоненте, где нужна кнопка, всталяем <Button>**Текст вашей кнопки**</Button>.
+// В пропы кнопке передаём "active" - если нужна рыжая кнопка.
+// В пропы также передаём size="short" - для кнопки логина и регистрации
+//                        size="long" - кнопка Start losing your weight
+//                        size = "addBtn" - кнопка Add
+//                        size = "addRoundBtn" - круглая кнопка Add.
+// Пример:
 
-Button.defaultProps = {
-  children: 'Default button',
-  onClick: () => {},
-  disabled: false,
-  active: false,
-};
+// import { Button } from "../../components";
+
+// export default function LoginPage(){
+//     return <Container>
+//     <SignInHeader>Sign in</SignInHeader>
+//     <Button size="short">Log in</Button>
+//     </Container>
+// }
+
+const Button = styled.button`
+background: ${props=>props.active?`var(--extra-color)`:`var(--text-extra-btn-color)`};
+color: ${props=>props.active?`var(--text-extra-btn-color)`:`var(--extra-color)`};
+border: ${props=>props.active?"none":"2px solid var(--extra-color)"};
+box-shadow: ${props=>props.active?"0px 4px 10px rgba(252, 132, 45, 0.5)":"none"};
+border-radius: 30px;
+cursor: pointer;
+padding: ${props=>{
+    switch(props.size){
+        case "short":
+            return `13px 51px;
+            `;
+        case "long":
+            return `13px 25px;
+            `
+        case "addBtn":
+            return `13px 46px;
+            `
+        case "addRoundBtn":
+            return `12.48px`
+        default: return `
+            13px 51px;
+        `
+    }
+}}
+`
+
+export default Button;
