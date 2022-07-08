20b31ab7ed6f02c8e89c4b51369111479c3cc309
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-07-09 00:58:54 +0300

    modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,49 +1,102 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { createPortal } from 'react-dom';
-import { OverlayModal, ModalBox } from './Modal.styled';
-
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeOnKeydown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeOnKeydown);
-  }
-
-  closeOnKeydown = (event) => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  closeOnBackdrop = (event) => {
-    if (event.target === event.currentTarget) {
-      this.props.onClose();
-    }
-  };
-  handleKeyDown = (e) => {
-    if (e.code === 'ArrowLeft') {
-      this.props.onLeft();
-    } else if (e.code === 'ArrowRight') {
-      this.props.onRight();
-    }
-  };
-  render() {
-    const modalRoot = document.getElementById('modal-root');
-    return createPortal(
-      <OverlayModal onClick={this.closeOnBackdrop}>
-        <ModalBox>{this.props.children}</ModalBox>
-      </OverlayModal>,
-      modalRoot
-    );
-  }
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import { RiCloseFill } from "react-icons/ri";
+import {
+  ModalOverlay,
+  ModalWrapper,
+  ModalContainer,
+  ModalContent,
+} from "./Modal.styled";
+import { Title, Text, Line, List, Item } from "./ModalContent.styled";
+import { ButtonClose } from "./ModalContent.styled";
+const Modal = ({ isShowing, hide, children }) => {
+  return isShowing
+    ? ReactDOM.createPortal(
+        <React.Fragment>
+          <ModalOverlay />
+          <ModalWrapper onClick={hide}>
+            <ModalContainer>
+              <ButtonClose
+                type="button"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={hide}
+              >
+                <RiCloseFill size={23} />
+              </ButtonClose>
+              <ModalContent>
+                {/* {children} */}
+                <Title>
+                  Your recommended daily
+                  <br /> calorie intake is
+                </Title>
+                <Text>
+                  2800 cal
+                  {/* кількість каллорій (приходить з бека) */}
+                </Text>
+                <Line />
+                <List>
+                  Foods you should not eat
+                  <Item>список (приходить з бека)</Item>
+                </List>
+              </ModalContent>
+              <button>Start losing weight</button>
+            </ModalContainer>
+          </ModalWrapper>
+        </React.Fragment>,
+        document.body
+      )
+    : null;
+};
 
 export default Modal;
 
-Modal.propTypes = {
-  children: PropTypes.node,
-  onClose: PropTypes.func,
-  src: PropTypes.string,
-  tags: PropTypes.string,
-};
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { createPortal } from 'react-dom';
+// import { OverlayModal, ModalBox } from './Modal.styled';
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.closeOnKeydown);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.closeOnKeydown);
+//   }
+
+//   closeOnKeydown = (event) => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   closeOnBackdrop = (event) => {
+//     if (event.target === event.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+//   handleKeyDown = (e) => {
+//     if (e.code === 'ArrowLeft') {
+//       this.props.onLeft();
+//     } else if (e.code === 'ArrowRight') {
+//       this.props.onRight();
+//     }
+//   };
+//   render() {
+//     const modalRoot = document.getElementById('modal-root');
+//     return createPortal(
+//       <OverlayModal onClick={this.closeOnBackdrop}>
+//         <ModalBox>{this.props.children}</ModalBox>
+//       </OverlayModal>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// export default Modal;
+
+// Modal.propTypes = {
+//   children: PropTypes.node,
+//   onClose: PropTypes.func,
+//   src: PropTypes.string,
+//   tags: PropTypes.string,
+// };
--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,19 +1,70 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const OverlayModal = styled.div`
+const ModalOverlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 1040;
   width: 100vw;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 1200;
+  background: rgba(33, 33, 33, 0.12);
 `;
-export const ModalBox = styled.div`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
-  overflow: auto;
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1050;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
 `;
+
+const ModalContainer = styled.div`
+  z-index: 100;
+  background: white;
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  padding-top: 40px;
+  padding-bottom: 116px;
+  padding-left: 20px;
+  padding-right: 20px;
+  @media only screen and (min-width: 769px) {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 672px;
+    height: 573px;
+    padding-top: 64px;
+    padding-bottom: 81px;
+    padding-left: 171px;
+    padding-right: 171px;
+  }
+`;
+
+const ModalContent = styled.div`
+  color: black;
+`;
+export { ModalContainer, ModalContent, ModalOverlay, ModalWrapper };
+
+// export const OverlayModal = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100vw;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: rgba(0, 0, 0, 0.8);
+//   z-index: 1200;
+// `;
+// export const ModalBox = styled.div`
+//   max-width: calc(100vw - 48px);
+//   max-height: calc(100vh - 24px);
+//   overflow: auto;
+// `;
