38c53dcbe04d1b9b7752106204803c8df78accd2
AliVik <85441270+AliVik@users.noreply.github.com>
2022-07-09 23:55:56 +0300

    add section component with reexport

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,5 @@
+import { SectionWrapper } from './Section.styled';
+
+export default function Section({ children }) {
+  return <SectionWrapper>{children}</SectionWrapper>;
+}
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SectionWrapper = styled.section`
+  padding-top: 40px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 160px;
+  }
+`;
