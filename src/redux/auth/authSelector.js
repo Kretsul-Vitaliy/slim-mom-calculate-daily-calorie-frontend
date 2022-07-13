93ae46a92fca0f00081ce011c6b28d9ca56fbf60
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-07-13 12:42:37 +0300

    fix bugs selectors

--- src/redux/auth/authSelector.js
+++ src/redux/auth/authSelector.js
@@ -1,3 +1,3 @@
 export const getIsAuthenticated = state => state.auth.isLogged;
-export const getIsUserAuthorizate = state => state.auth.token;
+// export const getIsUserAuthorizate = state => state.auth.token;
 export const getLoading = state => state.auth.isLoading;
