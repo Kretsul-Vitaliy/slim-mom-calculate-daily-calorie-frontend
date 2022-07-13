393171d6bb741e99abad28dd81dd05cd64e7040d
Хомылев Яков <yakoventii@gmail.com>
2022-07-13 11:42:06 +0300

    update

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,7 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { getIsUserAuthorizate } from '../../redux/auth/authSelector';
+
 
 export default function PrivateRoute({ children, redirectTo = '/' }) {
-  const token = useSelector((state) => state.auth.token);
+  const token = useSelector(getIsUserAuthorizate);
   return token ? children : <Navigate to={redirectTo} />;
 }
