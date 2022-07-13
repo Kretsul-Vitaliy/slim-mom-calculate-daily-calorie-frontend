71bbf76ce06462e898dcac7128d57b5d54ed346c
Kretsul-Vitaliy <84912966+Kretsul-Vitaliy@users.noreply.github.com>
2022-07-13 08:45:15 +0000

    Deploying to gh-pages from @ Kretsul-Vitaliy/slim-mom-calculate-daily-calorie-frontend@b14255a99240a22f7bdf58dec46f063c149295da 🚀

--- static/js/Private__Route.47a30e3e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkslim_mom_calculate_daily_calorie_frontend=self.webpackChunkslim_mom_calculate_daily_calorie_frontend||[]).push([[406],{3128:function(e,n,r){r.r(n),r.d(n,{default:function(){return i}});var t=r(9434),c=r(6871),u=r(3329);function i(e){var n=e.children,r=e.redirectTo,i=void 0===r?"/":r;return(0,t.v9)((function(e){return e.user.token}))?n:(0,u.jsx)(c.Fg,{to:i})}}}]);
-//# sourceMappingURL=Private__Route.47a30e3e.chunk.js.map
