47e10e234f5ab21e36e8742e3fbfab01cb41ff24
Zamder13 <Zamder@ukr.net>
2022-07-14 20:22:04 +0300

    okay

--- src/components/RacionSideBar/RacionSideBar.jsx
+++ src/components/RacionSideBar/RacionSideBar.jsx
@@ -6,16 +6,10 @@ import { BoxBg, PositionConteiner } from './RacionSideBar.styled.jsx';
 const RacionSideBar = ({ name = 'riko' }) => {
   return (
     <BoxBg>
-      {/* <StyledConteiner> */}
       <PositionConteiner>
-        {/* <User>
-          <p>{name}</p>
-          <button type="button">Exit</button>
-        </User> */}
         <DailyCalorieIntake />
         <DailyNotRecommendedFood />
       </PositionConteiner>
-      {/* </StyledConteiner> */}
     </BoxBg>
   );
 };
