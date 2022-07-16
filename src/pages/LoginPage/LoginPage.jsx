7dc9e802183855e915d98dc4d187a27c74c1b4e1
AliVik <85441270+AliVik@users.noreply.github.com>
2022-07-16 22:41:54 +0300

    add loader to login page, fix paths

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,20 +1,26 @@
 import { useTranslation } from 'react-i18next';
-import Container from '../../components/Container';
-import LoginForm from '../../components/LoginForm';
+import {useSelector} from "react-redux";
+import {Container,LoginForm,Section,Loader} from '../../components';
+
 import { SignInHeader } from './LoginPage.styled';
-import Section from '../../components/Section';
+import { getLoading } from '../../redux/auth/authSelector';
 
 const LoginPage = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+  const isLoading = useSelector(getLoading);
+
   return (
-    <>
+ 
       <Section>
         <Container>
+          {isLoading?<Loader/>:<>
           <SignInHeader>{t?.("auth.signInTitle")}</SignInHeader>
           <LoginForm />
+          </>}
+          
         </Container>
       </Section>
-    </>
+   
   );
 };
 
