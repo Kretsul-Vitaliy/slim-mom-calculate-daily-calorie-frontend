97e632cfdfa9a96d9a8465b841c2a31e773cd2c4
AliVik <85441270+AliVik@users.noreply.github.com>
2022-07-17 23:00:46 +0300

    add bottom padding to the section

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const SectionWrapper = styled.section`
   padding-top: 40px;
+  padding-bottom: 40px;
 
   @media screen and (min-width: 768px) {
     padding-top: 160px;
