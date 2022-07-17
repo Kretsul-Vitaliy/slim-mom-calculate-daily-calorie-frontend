224ee07e633bf2daf53c2ec074b6292778d12c2f
MaksStelmakh <step4ick2017@gmail.com>
2022-07-17 22:23:09 +0300

    update bugs

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -51,66 +51,67 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <>
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-        <GlobalStyle />
-        <Suspense fallback={<Loader />}>
-          <SwitcherButton switchTheme={setTheme} />
-          <Header />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <PublicRoute redirectTo="/calculator">
-                  <MainPage />
-                </PublicRoute>
-              }
-            ></Route>
-            <Route
-              path="login"
-              element={
-                <PublicRoute restricted redirectTo="/calculator">
-                  <LoginPage />
-                </PublicRoute>
-              }
-            ></Route>
-            <Route
-              path="signup"
-              element={
-                <PublicRoute restricted redirectTo="/calculator">
-                  <RegistrationPage />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="diary"
-              element={
-                <PrivateRoute redirectTo="/login">
-                  <DiaryPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="calculator"
-              element={
-                <PublicRoute redirectTo="/login">
-                  <CalculatorPage />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="google"
-              element={
-                <PublicRoute restricted redirectTo="/login">
-                  <CalculatorPage />
-                </PublicRoute>
-              }
-            />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-          <AuthorizeGoogle />
-          <Outlet />
-        </Suspense>
+    <Container>
+      <BackgroundContainer>
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+          <GlobalStyle />
+          <Suspense fallback={<Loader />}>
+            <SwitcherButton switchTheme={setTheme} />
+            <Header />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <PublicRoute redirectTo="/calculator">
+                    <MainPage />
+                  </PublicRoute>
+                }
+              ></Route>
+              <Route
+                path="login"
+                element={
+                  <PublicRoute restricted redirectTo="/calculator">
+                    <LoginPage />
+                  </PublicRoute>
+                }
+              ></Route>
+              <Route
+                path="signup"
+                element={
+                  <PublicRoute restricted redirectTo="/calculator">
+                    <RegistrationPage />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="diary"
+                element={
+                  <PrivateRoute redirectTo="/login">
+                    <DiaryPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="calculator"
+                element={
+                  <PublicRoute redirectTo="/login">
+                    <CalculatorPage />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="google"
+                element={
+                  <PublicRoute restricted redirectTo="/login">
+                    <CalculatorPage />
+                  </PublicRoute>
+                }
+              />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+            <AuthorizeGoogle />
+            <Outlet />
+          </Suspense>
 
           <ToastContainer autoClose={2500} />
         </ThemeProvider>
