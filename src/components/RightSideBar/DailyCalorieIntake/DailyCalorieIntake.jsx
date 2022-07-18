14f812aa06e9b52ebd89e505cb83a1f7af2326de
Zamder13 <Zamder@ukr.net>
2022-07-19 00:31:54 +0300

    selector

--- src/components/RightSideBar/DailyCalorieIntake/DailyCalorieIntake.jsx
+++ src/components/RightSideBar/DailyCalorieIntake/DailyCalorieIntake.jsx
@@ -8,27 +8,39 @@ import {
 } from './DailyCalorieIntake.slyled.jsx';
 
 const DailyCalorieIntake = ({ date = `06/20/2020`, kcal = '000' }) => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <Wrapper>
-      <Title>{t?.('rightSideBar.summary')} {date}</Title>
+      <Title>
+        {t?.('rightSideBar.summary')} {date}
+      </Title>
       <List>
         <ListItem>
           <ListItemData>{t?.('rightSideBar.left')}</ListItemData>
-          <ListItemData>{kcal} {t?.('dpi.ccal')}</ListItemData>
+          <ListItemData>
+            {kcal} {t?.('dpi.ccal')}
+          </ListItemData>
         </ListItem>
         <ListItem>
           <ListItemData>{t?.('rightSideBar.consumed')}</ListItemData>
-          <ListItemData>{kcal} {t?.('dpi.ccal')}</ListItemData>
+          <ListItemData>
+            {kcal} {t?.('dpi.ccal')}
+          </ListItemData>
         </ListItem>
         <ListItem>
           <ListItemData>{t?.('rightSideBar.dailyRate')}</ListItemData>
-          <ListItemData>{kcal} {t?.('dpi.ccal')}</ListItemData>
+          <ListItemData>
+            {kcal} {t?.('dpi.ccal')}
+            {/* { kcalNorm  && {kcalNorm = '000'}} */}
+            {/* {cal === undefined ? '000' : cal} */}
+          </ListItemData>
         </ListItem>
         <ListItem>
           <ListItemData>{t?.('rightSideBar.persent')}</ListItemData>
-          <ListItemData>{kcal} {t?.('dpi.ccal')}</ListItemData>
+          <ListItemData>
+            {kcal} {t?.('dpi.ccal')}
+          </ListItemData>
         </ListItem>
       </List>
     </Wrapper>
--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -2,8 +2,12 @@
 import DailyCalorieIntake from './DailyCalorieIntake';
 import DailyNotRecommendedFood from './DailyNotRecommendedFood';
 import { BoxBg, PositionConteiner } from './RacionSideBar.styled.jsx';
+// import { useSelector } from 'react-redux/es/exports.js';
 
 const RightSideBar = ({ name = 'riko' }) => {
+  // const kcalNorm = useSelector(state => state.user.userInfo.userData.calories);
+  // const isLogged = useSelector(state => state.auth.isLogged);
+  // console.log(!isLogged);
   return (
     <BoxBg>
       <PositionConteiner>
