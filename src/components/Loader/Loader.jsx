f0d88aae86c665c604600c34073b2e0e9dc5bda2
AliVik <85441270+AliVik@users.noreply.github.com>
2022-07-19 12:23:43 +0300

    remove dynamic color from Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,10 @@
 import { ThreeDots } from 'react-loader-spinner';
 import { LoaderWrapper } from './Loader.styled';
 
-const Loader = props => {
-  const color = props.theme.linkTextColor;
+const Loader = () => {
   return (
     <LoaderWrapper>
-      <ThreeDots
-        height="100"
-        width="100"
-        color={`${color}`}
-        ariaLabel="loading"
-      />
+      <ThreeDots height="100" width="100" color="#1ee41e" ariaLabel="loading" />
     </LoaderWrapper>
   );
 };
