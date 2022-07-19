a61aadcf11b1f9f54feab4d6e3d774ad327c0c3a
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-07-19 08:41:26 +0300

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,13 +2,14 @@ import { ThreeDots } from 'react-loader-spinner';
 import { LoaderWrapper } from './Loader.styled';
 
 const Loader = props => {
-  const color = props.theme.linkTextColor;
+  // const color = props.theme.linkTextColor;
   return (
     <LoaderWrapper>
       <ThreeDots
         height="100"
         width="100"
-        color={`${color}`}
+        color="orange"
+        // color={`${color}`}
         ariaLabel="loading"
       />
     </LoaderWrapper>
